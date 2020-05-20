53141524c711670e86ff55a137f7b2490016725a
Christopher Lebovitz <w10012072@usm.edu>
2020-05-20 16:25:30 -0500

    created the main driving file that generates a white screen for the game

--- aliean_invasion.py
+++ aliean_invasion.py
@@ -8,19 +8,27 @@ class AlienInvasion:
     """Overall class to mange game assets and behavior."""
 
     def __init__(self):
-        """initilize the game, and create game resources."""
+        """initialize the game, and create game resources."""
         pygame.init()
-
+        # create the game screen with the size of 1200x800 then assign it to an attribute to call
+        # throughout the program
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Aliean Invasion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # whatch for keyboard and ouse events.
+            # watch for keyboard and mouse events.
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
+                    # if the exit button on screen is clicked close the program
                     sys.exit()
+
+            # redraw the screen during each pass throught the loop
+            self.screen.fill(self.bg_color)
 
             # make the most recently drawn screen visible.
             pygame.display.flip()
