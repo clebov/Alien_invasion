831e44bc20ebd56272e84981a8fbedc07e2e09c8
Christopher Lebovitz <w10012072@usm.edu>
2020-05-21 21:23:27 -0500

    updating code. working on ship trail issue

--- ship.py
+++ ship.py
@@ -1,6 +1,6 @@
 import pygame
 from settings import Settings
-import time
+
 
 class Ship:
     """A class to mange the ship."""
@@ -11,7 +11,7 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = Settings()
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/player-enemies/playerShip1_red.png')
+        self.image = pygame.image.load('images/player-enemies/ufoBlue.png')
         self.rect = self.image.get_rect()
 
         # create a flag to check if there is movement
@@ -21,16 +21,12 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        Clock = pygame.time.Clock()
-        self.delta = Clock.tick(60) / 1000
-
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
-            self.rect.x += 1 * self.delta
+            self.rect.x += 1
         if self.moving_left:
-            self.rect.x -= 1 * self.delta
-        self.rect.x = int(self.rect.x)
+            self.rect.x -= .5
 
     def blitme(self):
         """Draw the ship at its current location."""
